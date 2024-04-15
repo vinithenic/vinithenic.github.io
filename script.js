6eb708d5f78dd212aa1c8316424a853bd8f674f6
vinithenic <126568963+vinithenic@users.noreply.github.com>
2024-04-15 12:12:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log("Você é maior de idade.");
+    } else {
+        console.log("Você é menor de idade.");
+    }
+}
+
+// Exemplo de uso:
+verificarIdade(20); // Saída: "Você é maior de idade."
+verificarIdade(16); // Saída: "Você é menor de idade."
